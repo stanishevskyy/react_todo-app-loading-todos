4a8d72609e4913331267f30f2c62978098e32daf
Your_Name Your_Surname <your@email>
2025-01-20 22:35:30 +0200

    problems

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,1 +1,27 @@
-export const ErrorMessage = () => {};
+import React, { useEffect } from 'react';
+import { ErrorType } from '../../types/ErrorType';
+import classNames from 'classnames';
+
+type Props = {
+  errorMessageTodo: ErrorType;
+  setError: (value: ErrorType) => void;
+};
+
+export const ErrorMessage: React.FC<Props> = ({
+  errorMessageTodo,
+  setError,
+}) => {
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className={classNames(
+        'notification is-danger is-light has-text-weight-normal',
+        { hidden: !errorMessageTodo },
+      )}
+    >
+      <button data-cy="HideErrorButton" type="button" className="delete" />
+      {/* show only one message at a time */}
+      {errorMessageTodo}
+    </div>
+  );
+};
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,1 +1,69 @@
-export const Footer = () => {};
+import React from 'react';
+import { SortType } from '../../types/SortType';
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todos: Todo[];
+  sortTodoBy: SortType;
+  onClick: (value: SortType) => void;
+};
+
+export const Footer: React.FC<Props> = ({ todos, sortTodoBy, onClick }) => {
+  const visibileTodo = todos.filter(todo => !todo.completed);
+  const disabledBtn = todos.some(todo => todo.completed);
+
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="TodosCounter">
+        {visibileTodo.length} items left
+      </span>
+
+      {/* Active link should have the 'selected' class */}
+      <nav className="filter" data-cy="Filter">
+        <a
+          href="#/"
+          className={classNames('filter__link', {
+            selected: sortTodoBy === SortType.SORT_ALL,
+          })}
+          data-cy="FilterLinkAll"
+          onClick={() => onClick(SortType.SORT_ALL)}
+        >
+          All
+        </a>
+
+        <a
+          href="#/active"
+          className={classNames('filter__link', {
+            selected: sortTodoBy === SortType.SORT_ACTIVE,
+          })}
+          data-cy="FilterLinkActive"
+          onClick={() => onClick(SortType.SORT_ACTIVE)}
+        >
+          Active
+        </a>
+
+        <a
+          href="#/completed"
+          className={classNames('filter__link', {
+            selected: sortTodoBy === SortType.SORT_COMPLETED,
+          })}
+          data-cy="FilterLinkCompleted"
+          onClick={() => onClick(SortType.SORT_COMPLETED)}
+        >
+          Completed
+        </a>
+      </nav>
+
+      {/* this button should be disabled if there are no completed todos */}
+      <button
+        type="button"
+        className="todoapp__clear-completed"
+        data-cy="ClearCompletedButton"
+        disabled={!disabledBtn}
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
